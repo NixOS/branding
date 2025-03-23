f267881d2e4fa2f67576ece4155d7749bf201513
Daniel Baker <daniel.n.baker@gmail.com>
2025-03-22 21:05:17 -0700

    progress

--- ffpy.py
+++ ffpy.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import fontforge
@@ -12,27 +13,50 @@ class Character:
         # font_file=Path("./vegur_0701/Vegur-Regular.otf"),
         font_file=Path("./route159_110/Route159-Regular.otf"),
         scale=1,
+        flip_x=False,
+        flip_y=True,
         remove_bearing=True,
     ):
         self.font = fontforge.open(str(font_file))
         self.glyph = self.font[character]
-        self._transform_glyph(scale, remove_bearing)
+        self._transform_glyph(scale, flip_x, flip_y, remove_bearing)
 
-    def _transform_glyph(self, scale, remove_bearing):
+    def _transform_glyph(self, scale, flip_x, flip_y, remove_bearing):
+        self.glyph.transform(
+            (
+                (-1 if flip_x else 1) * scale,
+                0,
+                0,
+                (-1 if flip_y else 1) * scale,
+                0,
+                0,
+            )
+        )
+
         x_offset = 0
         if remove_bearing:
             x_offset = -self.glyph.left_side_bearing
 
         self.glyph.transform(
             (
-                scale,
+                1,
                 0,
                 0,
-                -scale,
+                1,
                 x_offset,
                 0,
             )
         )
+
+    @property
+    def glyph_width(self):
+        bbox = self.glyph.boundingBox()
+        return bbox[2] - bbox[0]
+
+    @property
+    def glyph_height(self):
+        bbox = self.glyph.boundingBox()
+        return bbox[3] - bbox[1]
 
     def get_glyph_path(self):
         foreground = self.glyph.foreground
@@ -102,21 +126,33 @@ class Character:
         )
 
 
+@dataclass
 class Characters:
-    def __init__(
-        self,
-        characters: list[str],
-        spacings: list[int],
-    ):
-        self.characters = [Character(elem) for elem in characters]
+    characters: list[Character]
+    spacings: list[int]
+
+    def __post_init__(self):
+        self._set_spacings()
+
+    def _set_spacings(self):
         x_offset = 0
-        for character, spacing in zip(self.characters, spacings):
+        for character, spacing in zip(self.characters, self.spacings):
             x_offset += spacing
             character.glyph.transform((1, 0, 0, 1, x_offset, 0))
             character_width = (
                 character.glyph.boundingBox()[2] - character.glyph.boundingBox()[0]
             )
             x_offset += character_width
+
+    @property
+    def boundingBox(self):
+        return [
+            f(elem)
+            for f, elem in zip(
+                (min, min, max, max),
+                list(zip(*(elem.glyph.boundingBox() for elem in self.characters))),
+            )
+        ]
 
     def make_svg(self):
         constants = {"size": 800, "scale": 1}
@@ -132,9 +168,87 @@ class Characters:
         )
 
 
-my_char = Characters(
-    characters=["N", "i", "x", "O", "S"],
-    spacings=[200, 90, 70, 50, 10],
+@dataclass
+class DimensionedCharacters(Characters):
+    reference_size: int = 1024
+
+    def __post_init__(self):
+        self._set_ref_size()
+        super().__post_init__()
+
+    def _set_ref_size(self):
+        for character in self.characters:
+            character.glyph.transform(
+                (
+                    self.reference_size / character.font.capHeight,
+                    0,
+                    0,
+                    self.reference_size / character.font.capHeight,
+                    0,
+                    0,
+                )
+            )
+
+    def make_dimensioned_svg(self):
+        constants = {"size": self.reference_size * 1.1, "scale": 1}
+        bbox = self.boundingBox
+
+        return svg.SVG(
+            viewBox=svg.ViewBoxSpec(
+                min_x=-constants["size"] * 0,
+                min_y=-constants["size"] * 1,
+                width=constants["size"] * 4,
+                height=constants["size"] * 1,
+            ),
+            elements=[
+                svg.Rect(  # TODO: delete
+                    x=0,
+                    y=-constants["size"],
+                    width=4 * constants["size"],
+                    height=constants["size"],
+                    fill="#8888ee",
+                ),
+            ]
+            + [elem.get_svg_element() for elem in self.characters]
+            + [
+                svg.Rect(
+                    x=bbox[0],
+                    y=bbox[1],
+                    width=bbox[2] - bbox[0],
+                    height=bbox[3] - bbox[1],
+                    stroke="black",
+                    stroke_width=4,
+                    stroke_dasharray=8,
+                    fill="transparent",
+                )
+            ],
+        )
+
+
+# my_char = Characters(
+#     characters=[
+#         Character("N"),
+#         Character("i", flip_x=True),
+#         Character("x"),
+#         Character("O"),
+#         Character("S"),
+#     ],
+#     spacings=[200, 90, 70, 50, 10],
+# )
+# with open(Path("blah.svg"), "w") as file:
+#     file.write(str(my_char.make_svg()))
+
+my_char_dim = DimensionedCharacters(
+    characters=[
+        Character("N"),
+        Character("i", flip_x=True),
+        Character("x"),
+        Character("O"),
+        Character("S"),
+    ],
+    spacings=[0, 124, 96, 70, 14],
+    # spacings=[0, 0, 0, 0, 0],
+    reference_size=1024,
 )
-with open(Path("blah.svg"), "w") as file:
-    file.write(str(my_char.make_svg()))
+with open(Path("blah-dim.svg"), "w") as file:
+    file.write(str(my_char_dim.make_dimensioned_svg()))
