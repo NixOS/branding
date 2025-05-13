1fe481e44b5f4e06ac8ccf07943fb86b65a92908
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-12 19:59:43 -0700

    nixos-branding.verification.verify-artifacts-clearspace: init

---
+++ package-sets/top-level/nixos-branding/verification/verify-artifacts-clearspace/package.nix
@@ -0,0 +1,38 @@
+{
+  jq,
+  nix,
+  nix-eval-jobs,
+  nix-output-monitor,
+  stdenv,
+  writeShellApplication,
+}:
+writeShellApplication {
+
+  name = "verify-artifacts-clearspace";
+
+  runtimeInputs = [
+    jq
+    nix
+    nix-eval-jobs
+    nix-output-monitor
+  ];
+
+  text = ''
+    nix-eval-jobs \
+      --flake .#hydraJobs.nixos-branding-artifacts-clearspace.${stdenv.hostPlatform.system} \
+      --constituents \
+      | \
+      jq \
+      -cr \
+      '.constituents + [.drvPath] | .[] | select(.!=null) + "^*"' \
+      | \
+      nom \
+      build \
+      --keep-going \
+      --no-link \
+      --print-out-paths \
+      --stdin \
+      "$@"
+  '';
+
+}
