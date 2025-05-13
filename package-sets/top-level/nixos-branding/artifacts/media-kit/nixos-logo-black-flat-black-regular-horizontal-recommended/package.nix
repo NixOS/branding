d9fda3d264bea315ca4ca6c946654a0172931466
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-12 22:15:38 -0700

    nixos-logo-black-flat-black-regular-horizontal-recommended: init

---
+++ package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logo-black-flat-black-regular-horizontal-recommended/package.nix
@@ -0,0 +1,24 @@
+{
+  python3,
+  route159,
+  runCommandLocal,
+}:
+runCommandLocal "nixos-logo"
+  {
+    script = ./script.py;
+
+    nativeBuildInputs = [
+      (python3.withPackages (ps: [ ps.nixoslogo ]))
+    ];
+
+    env.NIXOS_LOGOTYPE_FONT_FILE = "${route159}/share/fonts/opentype/route159/Route159-Regular.otf";
+
+    outputHash = "sha256-AQKmAJ9GdpbbQPr89sRWp5DepZINsUZ92y/nIoEHSPE=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  }
+  ''
+    python $script
+    mkdir $out
+    cp *.svg $out/
+  ''
