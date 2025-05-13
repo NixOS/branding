72306952272b2718adcbe0a44c509255cc694e89
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-12 21:08:39 -0700

    nixos-branding.artifacts.media-kit.nixos-logomark-black-flat-recommended: init

---
+++ package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logomark-black-flat-recommended/package.nix
@@ -0,0 +1,21 @@
+{
+  python3,
+  runCommandLocal,
+}:
+runCommandLocal "nixos-logomark"
+  {
+    script = ./script.py;
+
+    nativeBuildInputs = [
+      (python3.withPackages (ps: [ ps.nixoslogo ]))
+    ];
+
+    outputHash = "sha256-fseNvWqqzdUC8hf8JAa7gWjQ6RaoIJD4cBSar+2EzSM=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  }
+  ''
+    python $script
+    mkdir $out
+    cp *.svg $out/
+  ''
