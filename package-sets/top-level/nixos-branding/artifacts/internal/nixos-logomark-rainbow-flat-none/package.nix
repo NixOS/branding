678d16fbd159af0a2c397cf5e4bb7e115acca100
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-12 23:13:12 -0700

    nixos-branding.artifacts.internal.nixos-logomark-rainbow-flat-none: init

---
+++ package-sets/top-level/nixos-branding/artifacts/internal/nixos-logomark-rainbow-flat-none/package.nix
@@ -0,0 +1,21 @@
+{
+  python3,
+  runCommandLocal,
+}:
+runCommandLocal "nixos-logomark"
+  {
+    script = ./script.py;
+
+    nativeBuildInputs = [
+      (python3.withPackages (ps: [ ps.nixoslogo ]))
+    ];
+
+    outputHash = "sha256-a5B7v3N556r0rcdt/bm7Zsj6guTv2t0WsMmG/0qZwXg=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  }
+  ''
+    python $script
+    mkdir $out
+    cp *.svg $out/
+  ''
