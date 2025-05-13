91baaf189e1d6f45a7e69101e2e82a00941f5e50
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-12 21:18:07 -0700

    nixos-logo-default-gradient-black-regular-horizontal-recommended: add defaults
    Add default values for class instantiation.

--- package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logo-default-gradient-black-regular-horizontal-recommended/script.py
+++ package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logo-default-gradient-black-regular-horizontal-recommended/script.py
@@ -1,4 +1,21 @@
+from nixoslogo.core import (
+    DEFAULT_LOGOTYPE_SPACINGS_WITH_BEARING,
+    ClearSpace,
+    ColorStyle,
+    LogoLayout,
+    LogomarkColors,
+    LogotypeStyle,
+)
 from nixoslogo.logo import NixosLogo
 
-logo = NixosLogo()
+logo = NixosLogo(
+    clear_space=ClearSpace.RECOMMENDED,
+    logo_layout=LogoLayout.HORIZONTAL,
+    logomark_color_style=ColorStyle.GRADIENT,
+    logomark_colors=LogomarkColors.DEFAULT,
+    logotype_characters="NixOS",
+    logotype_color="black",
+    logotype_spacings=DEFAULT_LOGOTYPE_SPACINGS_WITH_BEARING,
+    logotype_style=LogotypeStyle.REGULAR,
+)
 logo.write_svg()
