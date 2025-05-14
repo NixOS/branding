0528f9bec68bded66548b11ed107d818c15f6518
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-14 00:02:50 -0700

    nixos-logomark-default-gradient-minimal: init

---
+++ package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logomark-default-gradient-minimal/package.nix
@@ -0,0 +1,21 @@
+{
+  python3,
+  runCommandLocal,
+}:
+runCommandLocal "nixos-logomark"
+  {
+    script = ./script.py;
+
+    nativeBuildInputs = [
+      (python3.withPackages (ps: [ ps.nixoslogo ]))
+    ];
+
+    outputHash = "sha256-G+NkD+lUXAsaiiBODq6TPYy7rgi1xDBPrcuqpWhhgMU=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  }
+  ''
+    python $script
+    mkdir $out
+    cp *.svg $out/
+  ''
