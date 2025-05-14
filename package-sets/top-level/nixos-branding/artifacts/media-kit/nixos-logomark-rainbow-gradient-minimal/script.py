49810821b76b0f277369447ab9cc3193968722f6
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-14 00:05:26 -0700

    nixos-logomark-rainbow-gradient-minimal: init

---
+++ package-sets/top-level/nixos-branding/artifacts/media-kit/nixos-logomark-rainbow-gradient-minimal/script.py
@@ -0,0 +1,11 @@
+from nixoslogo.core import ClearSpace, ColorStyle, LogomarkColors
+from nixoslogo.logomark import Lambda, Logomark
+
+ilambda = Lambda()
+logomark = Logomark(
+    ilambda=ilambda,
+    colors=LogomarkColors.RAINBOW,
+    color_style=ColorStyle.GRADIENT,
+    clear_space=ClearSpace.MINIMAL,
+)
+logomark.write_svg()
