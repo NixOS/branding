9c03ef35a3cadb92235131a63c76d2c4f7d702a8
Daniel Baker <daniel.n.baker@gmail.com>
2025-05-16 19:55:28 -0700

    nixos-branding.verification.verify-nixos-branding-all: init

---
+++ package-sets/top-level/nixos-branding/verification/verify-nixos-branding-all/package.nix
@@ -0,0 +1,38 @@
+{
+  jq,
+  nix,
+  nix-eval-jobs,
+  nix-output-monitor,
+  stdenv,
+  writeShellApplication,
+}:
+writeShellApplication {
+
+  name = "verify-nixos-branding";
+
+  runtimeInputs = [
+    jq
+    nix
+    nix-eval-jobs
+    nix-output-monitor
+  ];
+
+  text = ''
+    nix-eval-jobs \
+      --flake .#hydraJobs.nixos-branding-all.${stdenv.hostPlatform.system} \
+      --constituents \
+      | \
+      jq \
+      -cr \
+      '.constituents + [.drvPath] | .[] | select(.!=null) + "^*"' \
+      | \
+      nom \
+      build \
+      --keep-going \
+      --no-link \
+      --print-out-paths \
+      --stdin \
+      "$@"
+  '';
+
+}
