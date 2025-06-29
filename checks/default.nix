3a3ee35e6eab2227cf526035498944c2eb39562c
Daniel Baker <daniel.n.baker@gmail.com>
2025-06-29 09:49:09 -0700

    nix.checks: remove derivitons
    We don't derivation builds in the checks. We will catch these in
    workflows.

--- checks/default.nix
+++ checks/default.nix
@@ -1,12 +1,6 @@
 inputs:
 inputs.self.library.defaultSystems (system: {
 
-  inherit (inputs.self.legacyPackages.${system}.nixos-branding)
-    nixos-branding-guide
-    nixos-logo-all-variants
-    nixos-media-kit
-    ;
-
   formatting = inputs.self.formatterModule.${system}.config.build.check inputs.self;
 
   pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
