6ec279c0b2088d1dc2e292a7ba60874d659e7016
Daniel Baker <daniel.n.baker@gmail.com>
2025-07-12 13:11:51 -0700

    overlays.add-typix-library: semi-automate updates
    Added conditional warnings for both typst and qpdf so when their
    upstream versions are past a certain point, warning messages are thrown.

--- overlays/add-typix-library/overlay.nix
+++ overlays/add-typix-library/overlay.nix
@@ -11,6 +11,14 @@ let
     composeManyExtensions
     ;
 
+  inherit (lib.strings)
+    versionOlder
+    ;
+
+  inherit (lib.trivial)
+    warnIf
+    ;
+
 in
 
 composeManyExtensions [
@@ -23,89 +31,104 @@ composeManyExtensions [
     final: prev:
 
     {
-      typix-lib = prev.typix-lib.overrideScope (
-        finalScope: prevScope: {
-
-          # TODO @djacu remove this once upstream qpdf reaches version 12
-          qpdf = final.qpdf;
-
-          buildDeterministicTypstProject = finalScope.callPackage (
+      typix-lib =
+        let
+          versionThreshold = "0.13.1";
+          currentTypstVersion = final.typst.version;
+        in
+        prev.typix-lib.overrideScope (
+          finalScope: prevScope:
+          warnIf (versionOlder versionThreshold currentTypstVersion)
+            ''
+              The current version of typst is ${currentTypstVersion} which is older than version ${versionThreshold}.
+              This overlay was created because typst version ${versionThreshold} did not deterministically create PDF files.
+              Specifically, certain color data in SVG files were not being embedded in the SVG file deterministically.
+              Try removing this overlay and see if the NixOS Branding Guide can be built and then re-built deterministically.
+              If it can, remove this overlay. If not, bump the version threshold.
+            ''
             {
-              lib,
-              mkTypstDerivation,
-              ocamlPackages,
-              qpdf,
-              typstOptsFromArgs,
-            }:
-            args@{
-              typstCompileCommand ? "typst compile",
-              typstSource ? "main.typ",
-              ...
-            }:
-            let
 
-              inherit (lib.attrsets)
-                removeAttrs
-                ;
+              qpdf = warnIf (versionOlder "12" prev.qpdf.version) ''
+                Upstream qpdf has reached version 12. This override and the associated derivation definition can be removed.
+              '' final.qpdf;
 
-              inherit (lib.strings)
-                escapeShellArg
-                removeSuffix
-                ;
+              buildDeterministicTypstProject = finalScope.callPackage (
+                {
+                  lib,
+                  mkTypstDerivation,
+                  ocamlPackages,
+                  qpdf,
+                  typstOptsFromArgs,
+                }:
+                args@{
+                  typstCompileCommand ? "typst compile",
+                  typstSource ? "main.typ",
+                  ...
+                }:
+                let
 
-              typstOptsString = args.typstOptsString or (typstOptsFromArgs args);
+                  inherit (lib.attrsets)
+                    removeAttrs
+                    ;
 
-              cleanedArgs = removeAttrs args [
-                "typstCompileCommand"
-                "typstOpts"
-                "typstOptsString"
-                "typstOutput"
-                "typstSource"
-              ];
+                  inherit (lib.strings)
+                    escapeShellArg
+                    removeSuffix
+                    ;
 
-              outfileName = "${removeSuffix ".typ" typstSource}";
+                  typstOptsString = args.typstOptsString or (typstOptsFromArgs args);
 
-              nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [
-                ocamlPackages.cpdf
-                qpdf
-              ];
+                  cleanedArgs = removeAttrs args [
+                    "typstCompileCommand"
+                    "typstOpts"
+                    "typstOptsString"
+                    "typstOutput"
+                    "typstSource"
+                  ];
 
-            in
-            mkTypstDerivation (
-              cleanedArgs
-              // {
-                inherit
-                  nativeBuildInputs
-                  ;
+                  outfileName = "${removeSuffix ".typ" typstSource}";
 
-                buildPhaseTypstCommand =
-                  args.buildPhaseTypstCommand or ''
-                    ${typstCompileCommand} \
-                      ${typstOptsString} \
-                      ${escapeShellArg typstSource} \
-                      ${outfileName}-compile.pdf
+                  nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [
+                    ocamlPackages.cpdf
+                    qpdf
+                  ];
 
-                    # need to remove the trailer ID so it can be recreated deterministically
-                    cpdf \
-                      -i ${outfileName}-compile.pdf \
-                      -remove-id \
-                      -o ${outfileName}-no-meta.pdf
+                in
+                mkTypstDerivation (
+                  cleanedArgs
+                  // {
+                    inherit
+                      nativeBuildInputs
+                      ;
 
-                    qpdf \
-                      ${outfileName}-no-meta.pdf \
-                      --remove-metadata \
-                      --deterministic-id \
-                      ${outfileName}.pdf
+                    buildPhaseTypstCommand =
+                      args.buildPhaseTypstCommand or ''
+                        ${typstCompileCommand} \
+                          ${typstOptsString} \
+                          ${escapeShellArg typstSource} \
+                          ${outfileName}-compile.pdf
 
-                    mkdir $out
-                    cp ${outfileName}.pdf $out/
-                  '';
-              }
-            )
-          ) { };
+                        # need to remove the trailer ID so it can be recreated deterministically
+                        cpdf \
+                          -i ${outfileName}-compile.pdf \
+                          -remove-id \
+                          -o ${outfileName}-no-meta.pdf
 
-        }
-      );
+                        qpdf \
+                          ${outfileName}-no-meta.pdf \
+                          --remove-metadata \
+                          --deterministic-id \
+                          ${outfileName}.pdf
+
+                        mkdir $out
+                        cp ${outfileName}.pdf $out/
+                      '';
+                  }
+                )
+              ) { };
+
+            }
+        );
     })
 
 ]
