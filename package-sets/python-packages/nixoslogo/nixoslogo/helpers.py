a207378cb947a04c7f22d05e8de726ae89ebc318
Daniel Baker <daniel.n.baker@gmail.com>
2025-10-10 11:49:38 -0700

    python3Packages.nixoslogo: add significant figure rounding function
    Add a rounding function and use where lambda points are generated. This
    helps resolves system platform differences.

--- package-sets/python-packages/nixoslogo/nixoslogo/helpers.py
+++ package-sets/python-packages/nixoslogo/nixoslogo/helpers.py
@@ -1,5 +1,5 @@
 import hashlib
-import math
+from math import floor, log10, sqrt
 
 import jsonpickle
 
@@ -16,7 +16,7 @@ def arc_sagitta(point1, point2, radius, large, sweep):
     chord_normal = unit_chord.normal()
     chord_length = chord.length()
 
-    sagitta = radius - math.sqrt(radius**2 - chord_length**2 / 4)
+    sagitta = radius - sqrt(radius**2 - chord_length**2 / 4)
 
     arc_center = (
         chord_midpoint
@@ -28,3 +28,10 @@ def arc_sagitta(point1, point2, radius, large, sweep):
     arc_midpoint = arc_center + radius * (+1 if sweep else -1) * chord_normal
     arc_midpoint_vector = (arc_midpoint - arc_center).normalize()
     return (arc_midpoint, arc_midpoint_vector)
+
+
+def round_to_sigfig(x: int | float, s: int = 12):
+    if x == 0:
+        return 0
+    else:
+        return round(x, s - 1 - int(floor(log10(abs(x)))))
