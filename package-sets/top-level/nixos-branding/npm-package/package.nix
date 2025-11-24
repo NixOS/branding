0ff7230254589567f195f632be28fff340e29341
Daniel Baker <dan@djacu.dev>
2025-11-23 20:33:13 -0800

    nixos-branding.npm-package: use the color palette tailwind package

--- package-sets/top-level/nixos-branding/npm-package/package.nix
+++ package-sets/top-level/nixos-branding/npm-package/package.nix
@@ -1,42 +1,17 @@
 {
   all-artifacts,
   lib,
-  nixos-color-palette,
+  nixos-color-palette-tailwind,
   nodePackages,
   stdenvNoCC,
 }:
 
 let
 
-  inherit (builtins)
-    foldl'
-    toString
-    ;
-
-  inherit (lib.attrsets)
-    attrValues
-    listToAttrs
-    mapAttrs
-    mapAttrs'
-    nameValuePair
-    ;
-
-  inherit (lib.lists)
-    map
-    ;
-
   inherit (lib.strings)
-    concatMapStringsSep
     readFile
-    replaceStrings
-    substring
     toJSON
-    toLower
     trim
-    ;
-
-  inherit (lib.trivial)
-    importTOML
     ;
 
   npmPackageMetadata = {
@@ -69,36 +44,6 @@ let
     ];
   };
 
-  toKebapCase = x: toLower (replaceStrings [ " " ] [ "-" ] x);
-
-  arrToOklch = arr: "oklch(${concatMapStringsSep " " toString arr})";
-
-  colorsFile = importTOML ../nixos-color-palette/colors.toml;
-
-  mapPalettes = mapAttrs (
-    paletteGroupName: paletteGroupValue:
-    listToAttrs (
-      map (
-        member:
-        let
-          mappedTints = mapAttrs' (
-            shadeName: shadeValue: nameValuePair (substring 1 2 shadeName) (arrToOklch shadeValue)
-          ) member.tints or { };
-        in
-        nameValuePair (toKebapCase "${paletteGroupName} ${member.name}") (
-          {
-            DEFAULT = arrToOklch member.value;
-          }
-          // mappedTints
-        )
-      ) paletteGroupValue
-    )
-
-  );
-  colors = foldl' (acc: elem: acc // elem) { } (
-    (attrValues (mapPalettes colorsFile.logos)) ++ (attrValues (mapPalettes colorsFile.palette))
-  );
-
 in
 
 stdenvNoCC.mkDerivation {
@@ -116,19 +61,15 @@ stdenvNoCC.mkDerivation {
   dontFixup = true;
 
   installPhase = ''
-    mkdir -p $out/colors
+    mkdir -p $out/{colors,artifacts}
+
     cat > $out/package.json <<EOF
     ${toJSON npmPackageMetadata}
     EOF
     ${nodePackages.prettier}/bin/prettier --write $out/package.json
 
-    mkdir -p $out/artifacts
     cp -RL -r ${all-artifacts}/* $out/artifacts/
-
-    cat > $out/colors/tailwind.js <<EOF
-    export default ${toJSON colors}
-    EOF
-    ${nodePackages.prettier}/bin/prettier --write $out/colors/tailwind.js
+    cp ${nixos-color-palette-tailwind}/tailwind.js $out/colors/tailwind.js
 
     cat > $out/.npmrc <<EOF
     //npm.pkg.github.com/:_authToken=$\{NODE_AUTH_TOKEN\}
